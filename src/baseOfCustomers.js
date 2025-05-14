8ee8423b9612aa11aceeb5d79b79744b7960367e
EMC-MSK-Officemag <153815004+EMC-MSK-Officemag@users.noreply.github.com>
2025-05-14 14:59:46 +0300

    14052025

--- src/baseOfCustomers.js
+++ src/baseOfCustomers.js
@@ -8,34 +8,4 @@ const serchData = document.getElementById("checkResult");
 
 checkDescription.innerHTML = "Получи свод по Клиенту:";
 
-// checkResultButton.addEventListener("click", (event) => {
-//   checkForAvailability();
-// });
 
-// Активация кнопки Проверки клиента
-
-// serchData.addEventListener("keyup", (event) => {
-//   event.key == "Enter" ? checkForAvailability() : true;
-// });
-
-//ВЫВОД информации о клиенте из массива объектов
-
-// const informationOutput = (infoBase) => {
-//   for (let index = 0; index < infoBase.length; index++) {
-//     if (serchData.value == Object.keys(infoBase[index])) {      
-//       return Object.values(infoBase[index])[0];
-//     }
-//   }  
-//   return "Нет";
-// };
-
-// // Функция проверки массива на наличие КОДА КЛИЕНТА
-// const checkForAvailability = (test) => {
-//   let itog = 0; // счётчик совпадений введенного значения с кодом из списка
-
-//   for (let index = 0; index < test.length; index++) {
-//     serchData.value == test[index] ? itog++ : false;
-//   }
-
-//   return itog;
-// };
---
+++ src/upload.js
@@ -0,0 +1,20 @@
+const uploadDate = async () => {
+  const upload = document.getElementById("upload");
+  const file = upload.files[0];
+  if (!file) {
+    alert("Выберите файл для загрузки");
+    return;
+  }
+  const formData = new FormData();
+  formData.append("myFile", file);
+
+  try {
+    await fetch(    
+      `http://127.0.0.1:3001/api/v1/upload`, {
+        method: "POST",
+        body: formData,
+      }
+    );
+
+  } catch (error) {}
+};
